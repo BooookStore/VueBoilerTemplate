37e066397c8d22f9f6552b12e3cf130ba1169461
bookstore <booookstore@icloud.com>
2018-07-09 20:56:22 +0900

    :tada: 初期構成

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>Hello, Vue !</p>
+        <p>Support</p>
+        <ul>
+            <li v-for="(content, key) in support" :key="key">{{content}}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Vue, Component} from "vue-property-decorator";
+
+    @Component
+    export default class Hello extends Vue {
+
+        // noinspection JSMethodCanBeStatic, JSUnusedGlobalSymbols
+        public data(): Object {
+            return {
+                message: "This is Vue Boiler Template.",
+                support: [
+                    "TypeScript",
+                    "scss",
+                    "sass",
+                    "decorator"
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
